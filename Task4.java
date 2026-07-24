9de4335b60fb8a5147ea47817be02aae38a3d283
pavanrajanala <pavanrajanala7@gmail.com>
2026-07-24 10:24:17 +0530

    Initial commit

---
+++ Task4.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int k = 3;
+
+        Arrays.stream(arr)
+                .sorted()
+                .skip(k - 1)
+                .findFirst()
+                .ifPresentOrElse(
+                        x -> System.out.println("Kth Smallest Element: " + x),
+                        () -> System.out.println("Invalid K")
+                );
+    }
+}
